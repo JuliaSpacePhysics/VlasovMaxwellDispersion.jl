89295af96fb0a0bd286c7bde950d179c9dcaa81d
Beforerr <zzj956959688@gmail.com>
2026-07-13 10:54:17 +0900

    perf: Chairmarks make bench fast

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,4 +1,5 @@
-using BenchmarkTools
+using ChairmarksForAirspeedVelocity
+import ChairmarksForAirspeedVelocity as CAV
 using VlasovMaxwellDispersion
 using VlasovMaxwellDispersion: contribution
 import SimpleNonlinearSolve as SNS
@@ -10,7 +11,7 @@ let g = SUITE["Maxwellian"]
     s = NormalizedSpecies(-1.0, 1.0, Maxwellian(vth_para = 0.9, vth_perp = 1.2))
     for kp in (0.1, 1.0)
         k = Wavenumber(kp, 0.4)
-        g["kperp=$kp"] = @benchmarkable contribution($s, 1.3 - 0.05im, $k)
+        g["kperp=$kp"] = CAV.@benchmarkable contribution($s, 1.3 - 0.05im, $k)
     end
 end
 
@@ -20,13 +21,13 @@ let g = SUITE["separable"]
     sep = SeparableVDF(Maxwellian(vth_para = vthp, vth_perp = vthq); para = (-14vthp, 14vthp), perp = 14vthq)
     for kp in (0.1, 1.0, 2.5)
         k = Wavenumber(kp, 0.4)
-        g["gaussian_kperp=$kp"] = @benchmarkable contribution($sep, 1.3 - 0.05im, $k)
+        g["gaussian_kperp=$kp"] = CAV.@benchmarkable contribution($sep, 1.3 - 0.05im, $k)
     end
     # Non-Gaussian parallel (kappa-like) × Gaussian perp
     sk = SeparableVDF(v -> exp(-v^2) / pi, u -> (1 + u^2 / 3)^(-2); para = (-30.0, 30.0), perp = 10.0)
     for kp in (1.0, 3.0)
         k = Wavenumber(kp, 0.4)
-        g["kappa_kperp=$kp"] = @benchmarkable contribution($sk, 1.2 - 0.05im, $k)
+        g["kappa_kperp=$kp"] = CAV.@benchmarkable contribution($sk, 1.2 - 0.05im, $k)
     end
 end
 
@@ -35,12 +36,12 @@ let g = SUITE["kappa"]
     for κ in (6, 6.00001)   # integer (residue) vs non-integer (₂F₁) parallel path
         s = NormalizedSpecies(-1.0, 0.7, BiKappa(vth_para = 0.9, vth_perp = 1.2, kappa = κ))
         for kp in (0.4, 2.0)
-            g["bikappa=$κ/kperp=$kp"] = @benchmarkable contribution($s, $ω, Wavenumber($kp, 0.3))
+            g["bikappa=$κ/kperp=$kp"] = CAV.@benchmarkable contribution($s, $ω, Wavenumber($kp, 0.3))
         end
     end
     pbk = NormalizedSpecies(-1.0, 0.7, ProductBiKappa(vth_para = 0.9, vth_perp = 1.2, kappa_para = 6, kappa_perp = 4))
     for kp in (0.4, 2.0)
-        g["product/kperp=$kp"] = @benchmarkable contribution($pbk, $ω, Wavenumber($kp, 0.3))
+        g["product/kperp=$kp"] = CAV.@benchmarkable contribution($pbk, $ω, Wavenumber($kp, 0.3))
     end
 end
 
@@ -53,8 +54,8 @@ let g = SUITE["Nonrelativistic/coupled"]
     ω = 1.2 + 0.05im
     for kp in (0.3, 1.0, 3.0)
         k = Wavenumber(kp, 0.4)
-        g["B_truncated/kperp=$kp"] = @benchmarkable contribution($sp, $ω, $k)
-        g["A_newberger/kperp=$kp"] = @benchmarkable contribution($sp, $ω, $k; closure = Newberger())
+        g["B_truncated/kperp=$kp"] = CAV.@benchmarkable contribution($sp, $ω, $k)
+        g["A_newberger/kperp=$kp"] = CAV.@benchmarkable contribution($sp, $ω, $k; closure = Newberger())
     end
 end
 
@@ -66,15 +67,15 @@ let g = SUITE["Relativistic"], regime = Relativistic()
         vdf = prepare(CoupledVDF(f0; para = (-L, L), perp = L, regime))
         ω = 0.3 + 0.05im
         k = Wavenumber(kp, 0.4)
-        g["coupled/A_newberger/μ=$μ/kperp=$kp"] = @benchmarkable contribution($vdf, $ω, $k; closure = Newberger())
-        g["coupled/B_truncated/μ=$μ/kperp=$kp"] = @benchmarkable contribution($vdf, $ω, $k)
+        g["coupled/A_newberger/μ=$μ/kperp=$kp"] = CAV.@benchmarkable contribution($vdf, $ω, $k; closure = Newberger())
+        g["coupled/B_truncated/μ=$μ/kperp=$kp"] = CAV.@benchmarkable contribution($vdf, $ω, $k)
 
         ppar = collect(range(-L, L, length = 81))
         pperp = collect(range(0.0, L, length = 61))
         F = [f0(u, w) for w in pperp, u in ppar]      # F[perp,par]
         vdf = GridVDF(pperp, ppar, F; regime)
-        g["gridvdf/μ=$μ/kperp=$kp"] = @benchmarkable contribution($vdf, $ω, $k)
-        g["gridvdf_coupled/μ=$μ/kperp=$kp"] = @benchmarkable contribution($vdf.coupled, $ω, $k)
+        g["gridvdf/μ=$μ/kperp=$kp"] = CAV.@benchmarkable contribution($vdf, $ω, $k)
+        g["gridvdf_coupled/μ=$μ/kperp=$kp"] = CAV.@benchmarkable contribution($vdf.coupled, $ω, $k)
     end
 end
 
@@ -83,14 +84,15 @@ let g = SUITE["local_solve"]
     k = Wavenumber(0.01, 0.5)
     prob = DispersionProblem(s, 0.6, k)
     h = 1.0e-3
-    g["muller_native"] = @benchmarkable solve($prob)
+    g["muller_native"] = CAV.@benchmarkable solve($prob)
     # Only complex-capable solvers qualify
     # bracketing methods need a real sign change Broyden/DFSane assume a real residual.
-    g["muller_sciml"] = @benchmarkable solve(ip, alg) setup = (
+    g["muller_sciml"] = CAV.@benchmarkable (
         ip = SNS.IntervalNonlinearProblem((ω, _) -> $prob.f(ω), ($prob.omega0 - $h, $prob.omega0 + $h));
-        alg = BNS.Muller($prob.omega0 + $h * im)
-    )
-    g["halley_sciml"] = @benchmarkable solve($prob, SNS.SimpleHalley())
+        alg = BNS.Muller($prob.omega0 + $h * im);
+        (ip, alg)
+    ) (x -> solve(x...))
+    g["halley_sciml"] = CAV.@benchmarkable solve($prob, SNS.SimpleHalley())
 end
 
 
@@ -98,12 +100,12 @@ let g = SUITE["branch_solve"]
     s = NormalizedSpecies(-1.0, 1.0, Maxwellian(vth_para = 0.9, vth_perp = 1.2))
     ks = [Wavenumber(0.01, kz) for kz in range(0.3, 0.8; length = 64)]
     prob = DispersionProblem(s, 0.6, ks)
-    g["arc_length/64"] = @benchmarkable solve($prob)
+    g["arc_length/64"] = CAV.@benchmarkable solve($prob)
 end
 
 
 let g = SUITE["global_solve"]
     s = NormalizedSpecies(-1.0, 1.0, Maxwellian(vth_para = 0.9, vth_perp = 1.2))
     prob = GlobalDispersionProblem(s, (0.2 - 0.4im, 1.5 + 0.1im), Wavenumber(0.01, 0.5))
-    g["Default/fixed_k"] = @benchmarkable solve($prob)
+    g["Default/fixed_k"] = CAV.@benchmarkable solve($prob)
 end
